41ac63d2ed261b8ea08c9e5750fd58b6a5316ff0
Farid Tabatabaie <farid.t@gmail.com>
2023-09-10 20:18:06 -0700

    Implement Point class and leverage it in Grid

--- 2022/day12.py
+++ 2022/day12.py
@@ -1,44 +1,65 @@
 from aocd import get_data
 
 
+class Point:
+    def __init__(self, coords: tuple[int, int], value: str) -> None:
+        self._x = coords[0]
+        self._y = coords[1]
+        self.value = value
+
+    @property
+    def x(self) -> int:
+        return self._x
+
+    @property
+    def y(self) -> int:
+        return self._y
+
+    @property
+    def coords(self) -> tuple[int, int]:
+        return self._x, self._y
+
+    @property
+    def height(self) -> int:
+        height = -1  # invalid height
+        if self.value.islower():
+            height = ord(self.value) - ord("a") + 1
+        elif self.value == "E":
+            height = ord("z") - ord("a") + 1
+        elif self.value == "S":
+            height = 1
+        return height
+
+    def __repr__(self) -> str:
+        return f"{self.coords}: {self.value}"
+
+
 class Grid:
     def __init__(self, raw_data: str) -> None:
         self.data = raw_data.splitlines()
         self.n_rows = len(self.data)
         self.n_cols = len(self.data[0])
 
-    def __getitem__(self, idx: tuple[int, int]) -> dict[tuple[int, int], str]:
-        return {(idx[0], idx[1]): self.data[idx[0]][idx[1]]}
+    def __getitem__(self, idx: tuple[int, int]) -> Point:
+        return Point(idx, self.data[idx[0]][idx[1]])
 
-    def _get_height(self, idx: tuple[int, int]) -> int:
-        height = -1
-        v = self[idx][idx]
-        if v.islower():
-            height = ord(v) - ord("a") + 1
-        elif v == "E":
-            height = ord("z") - ord("a") + 1
-        elif v == "S":
-            height = 1
-        return height
-
-    def get_traversable_neighbours(
-        self, idx: tuple[int, int]
-    ) -> list[dict[tuple[int, int], str]]:
+    def get_traversable_neighbours(self, idx: tuple[int, int]) -> list[Point]:
         neighbours = []
-        height = self._get_height(idx)
+        height = self[idx].height
+
         i, j = idx
         idx_up = (i - 1, j)
         idx_right = (i, j + 1)
         idx_down = (i + 1, j)
         idx_left = (i, j - 1)
 
-        if i > 0 and self._get_height(idx_up) - height <= 1:
+        if i > 0 and self[idx_up].height - height <= 1:
             neighbours.append(self[idx_up])
-        if i < self.n_rows - 1 and self._get_height(idx_down) - height <= 1:
+        if i < self.n_rows - 1 and self[idx_down].height - height <= 1:
             neighbours.append(self[idx_down])
-        if j > 0 and self._get_height(idx_left) - height <= 1:
+        if j > 0 and self[idx_left].height - height <= 1:
             neighbours.append(self[idx_left])
-        if j < self.n_cols - 1 and self._get_height(idx_right) - height <= 1:
+        if j < self.n_cols - 1 and self[idx_right].height - height <= 1:
             neighbours.append(self[idx_right])
 
         return neighbours
@@ -54,7 +75,9 @@ def main() -> None:
 
     test_idx = (1, 5)
     print(grid[test_idx])
-    print(grid.get_traversable_neighbours(test_idx))
+    valid_neighbs = grid.get_traversable_neighbours(test_idx)
+    print(valid_neighbs)
+    print([p.height for p in valid_neighbs])
 
 
 if __name__ == "__main__":
