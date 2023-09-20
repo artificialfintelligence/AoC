9ee0ce445084e3662008dbe5b231c4abeaeca11f
Farid Tabatabaie <farid.t@gmail.com>
2023-09-19 20:00:26 -0700

    Made the Packet class so I can repurpose __lt__()

--- 2022/day13.py
+++ 2022/day13.py
@@ -3,6 +3,44 @@
 import json
 
 
+class Packet:
+    def __init__(self, data):
+        self.data = data.copy()
+
+    def __lt__(self, other):
+        if not isinstance(other, Packet):
+            raise ValueError("Cannot compare Packet with non-Packet object.")
+        left = self.data
+        right = other.data
+        # print(f"{left=} , {right=}")
+        while len(left) > 0 and len(right) > 0:
+            l = left.pop(0)
+            r = right.pop(0)
+            # print(f"{l=} , {r=}")
+            if isinstance(l, int) and isinstance(r, int):
+                if l > r:
+                    return False
+                elif l < r:
+                    return True
+                else:
+                    continue
+            if isinstance(l, int):
+                l = [l]
+            if isinstance(r, int):
+                r = [r]
+            result = Packet(l) < Packet(r)
+            if result == None:
+                continue
+            else:
+                return result
+        if len(left) == 0 and len(right) > 0:
+            return True
+        elif len(right) == 0 and len(left) > 0:
+            return False
+        else:
+            return None
+
+
 def get_list_of_pairs(raw_data: str) -> list[tuple[list, list]]:
     data_list = raw_data.strip().split()
     # Can also use ast.literal_eval() instead of json.loads() below
@@ -11,47 +49,17 @@ def get_list_of_pairs(raw_data: str) -> list[tuple[list, list]]:
     return list_of_pairs
 
 
-def is_valid_list_pair(left: list, right: list) -> bool:
-    # print(f"{left=} , {right=}")
-    while len(left) > 0 and len(right) > 0:
-        l = left.pop(0)
-        r = right.pop(0)
-        # print(f"{l=} , {r=}")
-        if isinstance(l, int) and isinstance(r, int):
-            if l > r:
-                return False
-            elif l < r:
-                return True
-            else:
-                continue
-        if isinstance(l, int):
-            l = [l]
-        if isinstance(r, int):
-            r = [r]
-        result = is_valid_list_pair(l, r)
-        if result == None:
-            continue
-        else:
-            return result
-    if len(left) == 0 and len(right) > 0:
-        return True
-    elif len(right) == 0 and len(left) > 0:
-        return False
-    else:
-        return None
-
-
-def get_solutionn_part_1(pairs: list[tuple[list, list]]) -> int:
+def get_solution_part_1(pairs: list[tuple[list, list]]) -> int:
     sum_of_correct_indices = 0
     for idx, (left, right) in enumerate(pairs):
         # print(f"\n{idx=} , {left=} , {right=}")
-        # print(is_valid_list_pair(left, right))
-        if is_valid_list_pair(left.copy(), right.copy()):
+        # print(Packet(left) < Packet(right))
+        if Packet(left) < Packet(right):
             sum_of_correct_indices += idx + 1
     return sum_of_correct_indices
 
 
-def get_solution_part_2(raw_data: str) -> int:
+def get_solution_part_2(pairs: list[tuple[list, list]]) -> int:
     pass
 
 
@@ -60,9 +68,11 @@ def main() -> None:
 
     with open("data/day13.txt", "r") as f:
         data = f.read()
-
     pairs = get_list_of_pairs(data)
-    print(get_solutionn_part_1(pairs))
+    print(get_solution_part_1(pairs))
+
+    pairs_extended = pairs + [([[2]], [[6]])]
+    print(get_solution_part_1(pairs_extended))
 
 
 if __name__ == "__main__":
