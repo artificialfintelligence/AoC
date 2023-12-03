6b2fd760cb6c40b8a2b3d8bbb3081e06d3eac5d4
Farid Tabatabaie <farid.t@gmail.com>
2023-12-03 15:30:15 -0800

    Solve day 3 part 2 with tests

--- 2023/solvers/day03.py
+++ 2023/solvers/day03.py
@@ -36,7 +36,94 @@ def solve_part_1(data: list[str]) -> int:
 
 
 def solve_part_2(data: list[str]) -> int:
-    return 0
+    n_lines = len(data)
+    line_len = len(data[0])
+    sum_gear_ratios = 0
+    for line_num, line in enumerate(data):
+        for match in re.finditer(r"\*", line):
+            adjacent_numbers = []
+            idx = match.start()
+            search_start_idx = max(0, idx - 1)
+            search_end_idx = min(line_len, idx + 2)
+            print(f"Found {match.group()} on line {line_num} at index {idx}.")
+            if line_num > 0:
+                search_str = data[line_num - 1][search_start_idx:search_end_idx]
+
+                for m in re.finditer(r"\d+", search_str):
+                    seq = m.group()
+                    i = search_start_idx + m.start() - 1
+                    while i >= 0:
+                        prev_char = data[line_num - 1][i]
+                        if prev_char.isnumeric():
+                            seq = prev_char + seq
+                        else:
+                            break
+                        i -= 1
+
+                    i = search_start_idx + m.end()
+                    while i <= line_len:
+                        next_char = data[line_num - 1][i]
+                        if next_char.isnumeric():
+                            seq = seq + next_char
+                        else:
+                            break
+                        i += 1
+                    adjacent_numbers.append(int(seq))
+
+            if line_num < n_lines - 1:
+                search_str = data[line_num + 1][search_start_idx:search_end_idx]
+                for m in re.finditer(r"\d+", search_str):
+                    seq = m.group()
+                    i = search_start_idx + m.start() - 1
+                    while i >= 0:
+                        prev_char = data[line_num + 1][i]
+                        if prev_char.isnumeric():
+                            seq = prev_char + seq
+                        else:
+                            break
+                        i -= 1
+
+                    i = search_start_idx + m.end()
+                    while i < line_len:
+                        next_char = data[line_num + 1][i]
+                        if next_char.isnumeric():
+                            seq = seq + next_char
+                        else:
+                            break
+                        i += 1
+                    adjacent_numbers.append(int(seq))
+
+            if idx > 0:
+                if line[idx - 1].isnumeric():
+                    seq = line[idx - 1]
+                    i = idx - 2
+                    while i >= 0:
+                        prev_char = line[i]
+                        if prev_char.isnumeric():
+                            seq = prev_char + seq
+                        else:
+                            break
+                        i -= 1
+                    adjacent_numbers.append(int(seq))
+
+            if idx < line_len - 1:
+                if line[idx + 1].isnumeric():
+                    seq = line[idx + 1]
+                    i = idx + 2
+                    while i < line_len:
+                        next_char = line[i]
+                        if next_char.isnumeric():
+                            seq = seq + next_char
+                        else:
+                            break
+                        i += 1
+                    adjacent_numbers.append(int(seq))
+
+            if len(adjacent_numbers) == 2:
+                print(f"{adjacent_numbers = }\n")
+                sum_gear_ratios += adjacent_numbers[0] * adjacent_numbers[1]
+
+    return sum_gear_ratios
 
 
 def solve(data: list[str], part: int) -> int:
