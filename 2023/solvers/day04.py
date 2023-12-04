7ec40b742d7c8a2737471004689f12f3743f36c7
Farid Tabatabaie <farid.t@gmail.com>
2023-12-03 22:20:25 -0800

    Solve day 4 part 1 with tests

---
+++ 2023/solvers/day04.py
@@ -0,0 +1,67 @@
+# pylint: disable=missing-module-docstring, missing-docstring
+
+import re
+
+
+def process_data(data: list[str]) -> tuple[list[list[int]], list[list[int]]]:
+    """Parses raw input data into more readily usable format as described below.
+
+    Args:
+        data:
+            Raw data in the common "list of strings" format output by the
+            `data_io` module.
+            example:
+
+            ["Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+             "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19"]
+
+    Returns:
+        A tuple of two lists of lists of ints. The first element of the tuple is
+        all the winning numbers on the cards and the second elements is the list
+        of the numbers we have on the corresponding cards.
+        example:
+
+        (
+            [[41, 48, 83, 86, 17], [13, 32, 20, 16, 61]],
+            [[83, 86, 6, 31, 17, 9, 48, 53], [61, 30, 68, 82, 17, 32, 24, 19]]
+        )
+    """
+    nums_winning, nums_we_have = [], []
+    pattern = re.compile(r"^Card +\d+: +(.+) \| (.+)$")
+    for card in data:
+        matches = pattern.search(card)
+        if matches:
+            winning_numbers, our_numbers = matches.groups()
+            nums_winning.append([int(n) for n in winning_numbers.split()])
+            nums_we_have.append([int(n) for n in our_numbers.split()])
+    return nums_winning, nums_we_have
+
+
+def solve_part_1(
+    nums_winning: list[list[int]], nums_we_have: list[list[int]]
+) -> int:
+    total_points = 0
+    for card_idx, nums2check in enumerate(nums_we_have):
+        winning_set = set(nums_winning[card_idx])
+        match_count = sum(1 for n in nums2check if n in winning_set)
+        card_points = 2 ** (match_count - 1) if match_count else 0
+        # print(
+        #     f"Card #{card_idx+1} has {match_count} matches and is worth {card_points} points."
+        # )
+        total_points += card_points
+    return total_points
+
+
+def solve_part_2(
+    nums_winning: list[list[int]], nums_we_have: list[list[int]]
+) -> int:
+    return 0
+
+
+def solve(data: list[str], part: int) -> int:
+    nums_winning, nums_we_have = process_data(data)
+    if part == 1:
+        solution = solve_part_1(nums_winning, nums_we_have)
+    else:  # part == 2
+        solution = solve_part_2(nums_winning, nums_we_have)
+    return solution
---
+++ 2023/tests/test_day04.py
@@ -0,0 +1,49 @@
+# pylint: disable=missing-module-docstring, missing-docstring
+
+import os
+from pathlib import Path
+
+from data_io import load_data
+from solvers import day04
+
+test_data_file_path = os.path.join("data", f"{Path(__file__).stem}.txt")
+
+TEST_DATA = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+"""
+with open(test_data_file_path, "w", encoding="utf-8") as f:
+    f.write(TEST_DATA)
+
+EXPECTED_SOLUTION_PART_1_WITH_TEST_DATA = 13
+# EXPECTED_SOLUTION_PART_2_WITH_TEST_DATA = 0
+
+EXPECTED_SOLUTION_PART_1_WITH_REAL_DATA = 25571
+# EXPECTED_SOLUTION_PART_2_WITH_REAL_DATA = 0
+
+
+def test_part_1_with_test_data():
+    data = load_data(day=4, year=2023, is_testmode=True)
+    solution = day04.solve(data, part=1)
+    assert solution == EXPECTED_SOLUTION_PART_1_WITH_TEST_DATA
+
+
+# def test_part_2_with_test_data():
+#     data = load_data(day=4, year=2023, is_testmode=True)
+#     solution = day04.solve(data, part=2)
+#     assert solution == EXPECTED_SOLUTION_PART_2_WITH_TEST_DATA
+
+
+def test_part_1_with_real_data():
+    data = load_data(day=4, year=2023, is_testmode=False)
+    solution = day04.solve(data, part=1)
+    assert solution == EXPECTED_SOLUTION_PART_1_WITH_REAL_DATA
+
+
+# def test_part_2_with_real_data():
+#     data = load_data(day=4, year=2023, is_testmode=False)
+#     solution = day04.solve(data, part=2)
+#     assert solution == EXPECTED_SOLUTION_PART_2_WITH_REAL_DATA
