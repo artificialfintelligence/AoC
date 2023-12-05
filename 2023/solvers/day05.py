bec9fc814ecbeb457432099878cdad936c6af128
Farid Tabatabaie <farid.t@gmail.com>
2023-12-04 21:47:58 -0800

    WiP: Day05 part 1 data proc and skeleton

---
+++ 2023/solvers/day05.py
@@ -0,0 +1,55 @@
+# pylint: disable=missing-module-docstring, missing-docstring
+
+import re
+
+
+def process_data(
+    data: list[str],
+) -> tuple[list[int], dict[str : list[tuple[int, int, int]]]]:
+    """Parses raw input data into more readily usable format as described below.
+
+    Args:
+        data:
+            Raw data in the common "list of strings" format output by the
+            `data_io` module.
+
+    Returns:
+        A tuple of two elements, the first being the list of initial seeds and
+        the second being a dictionary of the required mappings, where the string
+        keys name the mapping and the values are lists of tuples of integers,
+        where each tuple corresponds to a (destination range start, source range
+        start, range length) as per the puzzle description.
+    """
+    init_seeds = [int(n) for n in data[0].replace("seeds: ", "").split()]
+    mappings = {}
+    curr_key = ""
+    for line in data[1:]:
+        if line and line[0].isalpha():
+            curr_key = line.replace(" map:\n", "")
+            mappings[curr_key] = []
+        elif line and line[0].isnumeric():
+            mappings[curr_key].append(tuple(int(x) for x in line.split()))
+    return (init_seeds, mappings)
+
+
+def solve_part_1(
+    init_seeds: list[int], mappings: dict[str : list[tuple[int, int, int]]]
+) -> int:
+    min_loc = 0
+    return min_loc
+
+
+def solve_part_2(
+    init_seeds: list[int], mappings: dict[str : list[tuple[int, int, int]]]
+) -> int:
+    result = 0
+    return result
+
+
+def solve(data: list[str], part: int) -> int:
+    init_seeds, mappings = process_data(data)
+    if part == 1:
+        solution = solve_part_1(init_seeds, mappings)
+    else:  # part == 2
+        solution = solve_part_2(init_seeds, mappings)
+    return solution
---
+++ 2023/tests/test_day05.py
@@ -0,0 +1,76 @@
+# pylint: disable=missing-module-docstring, missing-docstring
+
+import os
+from pathlib import Path
+
+from data_io import load_data
+from solvers import day04
+
+test_data_file_path = os.path.join("data", f"{Path(__file__).stem}.txt")
+
+TEST_DATA = """seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4
+"""
+with open(test_data_file_path, "w", encoding="utf-8") as f:
+    f.write(TEST_DATA)
+
+EXPECTED_SOLUTION_PART_1_WITH_TEST_DATA = 35
+# EXPECTED_SOLUTION_PART_1_WITH_REAL_DATA = 0
+
+# EXPECTED_SOLUTION_PART_2_WITH_TEST_DATA = 0
+# EXPECTED_SOLUTION_PART_2_WITH_REAL_DATA = 0
+
+
+def test_part_1_with_test_data():
+    data = load_data(day=4, year=2023, is_testmode=True)
+    solution = day04.solve(data, part=1)
+    assert solution == EXPECTED_SOLUTION_PART_1_WITH_TEST_DATA
+
+
+# def test_part_1_with_real_data():
+#     data = load_data(day=4, year=2023, is_testmode=False)
+#     solution = day04.solve(data, part=1)
+#     assert solution == EXPECTED_SOLUTION_PART_1_WITH_REAL_DATA
+
+
+# def test_part_2_with_test_data():
+#     data = load_data(day=4, year=2023, is_testmode=True)
+#     solution = day04.solve(data, part=2)
+#     assert solution == EXPECTED_SOLUTION_PART_2_WITH_TEST_DATA
+
+
+# def test_part_2_with_real_data():
+#     data = load_data(day=4, year=2023, is_testmode=False)
+#     solution = day04.solve(data, part=2)
+#     assert solution == EXPECTED_SOLUTION_PART_2_WITH_REAL_DATA
